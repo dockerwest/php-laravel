dd2afe735439718bc8d3d330f60703e3bc983948
Pieter Meyvaert <pieter.meyvaert@studioemma.com>
2017-02-23 13:17:56 +0100

    Added php-imagick to install

--- 7.0/scripts/install.sh
+++ 7.0/scripts/install.sh
@@ -5,6 +5,6 @@ set -e
 # install packages
 apt-get update
 apt-get install -y php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-sqlite3 \
-    php7.0-bz2 php7.0-xml php7.0-mbstring php7.0-zip php-redis
+    php7.0-bz2 php7.0-xml php7.0-mbstring php7.0-zip php-redis php7.0-imagick
 
 apt-get clean -y
--- 7.1/scripts/install.sh
+++ 7.1/scripts/install.sh
@@ -5,6 +5,6 @@ set -e
 # install packages
 apt-get update
 apt-get install -y php7.1-gd php7.1-mysql php7.1-mcrypt php7.1-sqlite3 \
-    php7.1-bz2 php7.1-xml php7.1-mbstring php7.1-zip php-redis
+    php7.1-bz2 php7.1-xml php7.1-mbstring php7.1-zip php-redis php7.1-imagick
 
 apt-get clean -y

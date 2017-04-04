84a85190d4509dedf9cde563ef0128ebd3495cdf
Pieter Meyvaert <pieter.meyvaert@studioemma.com>
2017-04-04 20:47:51 +0200

    Added mysql client for cli commands

--- 5.6/scripts/install.sh
+++ 5.6/scripts/install.sh
@@ -5,6 +5,6 @@ set -e
 # install packages
 apt-get update
 apt-get install -y php5.6-gd php5.6-mysql php5.6-mcrypt php5.6-sqlite3 \
-    php5.6-bz2 php5.6-xml php5.6-mbstring php5.6-zip php-redis php5.6-imagick
+    php5.6-bz2 php5.6-xml php5.6-mbstring php5.6-zip php-redis php5.6-imagick mysql-client
 
 apt-get clean -y

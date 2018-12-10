dc2fac24b57dc05fa9801caafc7a1793969a06ec
BlackEagle <ike.devolder@gmail.com>
2018-12-10 15:15:54 +0100

    add sodium as default extension
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- 7.2/scripts/install.sh
+++ 7.2/scripts/install.sh
@@ -5,7 +5,7 @@ set -e
 # install packages
 apt-get update
 
-extensions -i bz2 gd imagick mbstring mysql redis sqlite3 xml zip
+extensions -i bz2 gd imagick mbstring mysql redis sodium sqlite3 xml zip
 
 apt-get clean -y
 
